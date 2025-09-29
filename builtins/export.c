7b7f123354e8877abaa021c7d0a117e4641844c8
aniavg <aniavg2003@gmail.com>
2025-09-29 23:14:55 +0400

    export mostly done, need to fix some cases

--- builtins/export.c
+++ builtins/export.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
@@ -6,9 +6,9 @@
 /*   By: anavagya <anavgya@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 12:23:25 by anavagya          #+#    #+#             */
-/*   Updated: 2025/09/29 18:35:05 by anavagya         ###   ########.fr       */
+/*   Updated: 2025/09/29 23:03:09 by anavagya         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "builtins.h"
 
@@ -68,31 +68,99 @@ char	*if_env_value_exist(t_env *env, char *key)
 	return (NULL);
 }
 
+int	return_name_size_ex(char *arg)
+{
+	int	i;
+
+	i = 0;
+	if (!arg || !*arg)
+		return (-1);
+	while (arg[i])
+	{
+		if (arg[i] == '=')
+			return (i);
+		i++;
+	}
+	return (ft_strlen(arg));
+}
+
+char	*return_name_ex(char *arg)
+{
+	int	index;
+
+	if (!arg || !*arg)
+		return (NULL);
+	index = return_name_size_ex(arg);
+	if (index < 0)
+		return (NULL);
+	return (ft_substr(arg, 0, index));
+}
+
+char	*return_value_ex(char *arg)
+{
+	int	index;
+
+	if (!arg || !*arg)
+		return (NULL);
+	index = return_name_size(arg);
+	if (index < 0)
+		return (NULL);
+	return (ft_substr(arg, index + 1, ft_strlen(arg) - index - 1));
+}
+
 int	built_in_export(char **args, int argc, t_env **env)
 {
 	int		i;
+	char	*name;
 	char	*value;
+	char	*env_value;
 	char	**env_arr;
+	t_env	*tmp;
+	t_env	*head;
 
 	if (!env)
 		return (0);
-	i = 0;
-	env_arr = convert_to_array(env);
+	head = *env;
+	i = 1;
+	if (argc > 1)
+	{
+		while (args[i])
+		{
+			name = return_name_ex(args[i]);
+			value = return_value_ex(args[i]);
+			if (get_env_key_index(*env, name) == -1)
+			{
+				// printf("name = %s\n value = %s\n\n\n\n", name, value);
+				tmp = ft_lstnew(name, value);
+				ft_lstadd_back(env, tmp);
+			}
+			if (get_env_key_index(*env, name) != -1 && !if_env_value_exist(*env, name))
+			{
+				while (head)
+				{
+					if (ft_strcmp(head->name, name) == 0)
+						head->value = value;
+					head = head->next;
+				}
+			}
+			i++;
+		}
+	}
+	env_arr = convert_to_array(*env);
 	sort_in_alpha_order(env_arr);
 	if (argc == 1)
 	{
+		i = 0;
 		while (env_arr[i])
 		{
-			value = if_env_value_exist(env, env_arr[i]);
-			if (value)
-				printf("declare -x %s=\"%s\"\n", env_arr[i], value);
+			env_value = if_env_value_exist(*env, env_arr[i]);
+			if (env_value)
+				printf("declare -x %s=\"%s\"\n", env_arr[i], env_value);
 			else
 				printf("declare -x %s\n", env_arr[i]);
 			i++;
 		}
 	}
-	else
-	{}
 	ft_free(env_arr);
 	return (1);
 }
--- builtins/main.c
+++ builtins/main.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
@@ -6,9 +6,9 @@
 /*   By: anavagya <anavgya@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 13:18:51 by anavagya          #+#    #+#             */
-/*   Updated: 2025/09/29 18:09:36 by anavagya         ###   ########.fr       */
+/*   Updated: 2025/09/29 23:04:40 by anavagya         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "builtins.h"
 
@@ -19,11 +19,25 @@ int	main(int argc, char **argv, char **envp)
 	(void)argc;
 	(void)argv;
 	// char *str[] = {"echo", "-n", "-nn", "barev", "aziz", "-n", "jan", NULL};
-	char *str[] = {"export", NULL};
+	char *str[] = {"export", "zibil", NULL};
+	char *str1[] = {"export", "zibil=vay", NULL};
+	char *str_unset[] = {"unset", "zibil", NULL};
 	env = env_parse(envp);
+	built_in_export(str, 2, &env);
 	built_in_env(env);
 	// built_in_unset(str, &env);
 	printf("\n\n\n");
-	built_in_export(str, argc, env);
+	built_in_export(str, 1, &env);
+	printf("\n\n\n");
+	built_in_export(str1, 2, &env);
+	built_in_export(str, 1, &env);
+	printf("\n\n\n");
+	built_in_env(env);
+	built_in_unset(str_unset, &env);
+
+	printf("tpeeeeeenqqqqqqqqqq\n\n\n");
+	built_in_export(str, 1, &env);
+	printf("\n\n\n");
+	built_in_env(env);
 	return (0);
 }
