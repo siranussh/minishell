7223368a5081b22f2eb67f19ec56421e529d4eac
Ani Avagyan <anavagya@student.42yerevan.am>
2025-12-07 12:52:35 +0400

    fixed exit leaks

--- builtins/exit.c
+++ builtins/exit.c
@@ -1,29 +1,37 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anavagya <anavagya@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anavagya <anavgya@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 14:46:06 by anavagya          #+#    #+#             */
-/*   Updated: 2025/12/06 21:29:52 by anavagya         ###   ########.fr       */
+/*   Updated: 2025/12/07 12:48:23 by anavagya         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
 int	built_in_exit(int argc, char **args, t_data *data)
 {
 	long	status;
+	int		last_status;
 
 	if (!args || !*args)
 		return (-1);
 	write(STDERR_FILENO, "exit\n", 5);
 	if (argc == 1)
-		exit(data->exit_code);
+	{
+		last_status = data->exit_code;
+		if (data)
+			free_data(data);
+		exit(last_status);
+	}
 	if (!is_number(args[1]) || is_long_overflow(args[1]))
 	{
 		print_error("minishell: exit", args[1], "numeric argument required");
+		if (data)
+			free_data(data);
 		exit(2);
 	}
 	if (argc > 2)
@@ -32,5 +40,7 @@ int	built_in_exit(int argc, char **args, t_data *data)
 		return (1);
 	}
 	status = ft_atol(args[1]);
+	if (data)
+		free_data(data);
 	exit(status % 256);
 }
